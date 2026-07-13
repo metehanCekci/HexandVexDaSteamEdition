24cf0bba9e90e21bce0c2da9f1796ae6b61daaf7
Metehan <metehancekci9@gmail.com>
2026-07-13 23:38:23 +0300

    cae

--- Assets/Scripts/MagicTileType.cs
+++
@@ -1,8 +0,0 @@
-public enum MagicTileType
-{
-    Red,    // 2x damage
-    Blue,   // extended range movement
-    Green,  // +1 dice
-    Yellow, // +10 gold on step
-    Orange  // +1 extra move while standing, consumed on leave
-}
--- Assets/Scripts/UI/SellBoxDropZone.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-/// <summary>
-/// SellBox drop zone'unu işaretleyen tag component.
-/// PerkInventoryUI EndDrag sırasında bu component'i arar.
-/// </summary>
-public class SellBoxDropZone : MonoBehaviour { }
